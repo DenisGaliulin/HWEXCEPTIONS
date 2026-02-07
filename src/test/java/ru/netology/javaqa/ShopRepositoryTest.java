604303d8902b819d28fc46f4d8c0e09de14baa5f
Denis.test <denis.galiulin.a@yandex.ru>
2026-02-07 15:50:09 +0500

    Initial commit

---
+++ src/test/java/ru/netology/javaqa/ShopRepositoryTest.java
@@ -0,0 +1,58 @@
+package ru.netology.javaqa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ShopRepositoryTest {
+
+    private ShopRepository sr;
+    private Product product1;
+    private Product product2;
+    private Product product3;
+
+
+    @BeforeEach
+    public void setup() {
+        sr = new ShopRepository();
+
+        product1 = new Product(1, "Хлеб", 50);
+        product2 = new Product(2, "Молоко", 80);
+        product3 = new Product(3, "Сыр", 200);
+
+        sr.add(product1);
+        sr.add(product2);
+        sr.add(product3);
+    }
+
+    @Test
+    public void shouldRemoveById(){
+
+        Product[] productsBefore = sr.findAll();
+
+
+        try {
+            sr.removeById(2);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        Product[] productsAfter =sr.findAll();
+
+        Assertions.assertEquals(2, productsAfter.length);
+
+
+    }
+
+    @Test
+    public void shouldNotFoundException(){
+
+        Assertions.assertThrows(NotFoundException.class, () -> {
+            sr.removeById(4);
+        });
+    }
+
+
+}
